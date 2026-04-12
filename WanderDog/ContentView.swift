ad873a9f243ebf765fbdaa1f9e26a63b2d555b7d
Dennis <Dennisb723@gmail.com>
2026-04-12 12:24:44 -0500

    Initial Commit

---
+++ WanderDog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WanderDog
+//
+//  Created by Dennis Bowen on 4/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WanderDog/WanderDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  WanderDogApp.swift
+//  WanderDog
+//
+//  Created by Dennis Bowen on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct WanderDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
